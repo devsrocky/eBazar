9b36d60b6f6a91f0c9fbd94497aebf7cbd696c75
Rabiul Islam Rocky <152948126+devsrocky@users.noreply.github.com>
2026-03-09 12:11:51 +0600

    Add route for fetching invoice order list

--- src/route/api.js
+++ src/route/api.js
@@ -51,6 +51,7 @@ router.get('/RemoveCart/:cartID', Auth, CartController.RemoveCart)
 
 // INVOICE || routes
 router.post('/CreateInvoice', Auth, InvoiceController.CreateInvoice)
+router.get('/Invoice-order-list', Auth, InvoiceController.OrderList)
 router.get('/InvoiceList/:userId', Auth, InvoiceController.InvoiceList)
 
 
